53799a9e5ac42ec0cebe21c71845de2fa5709415
min.shi <mish@source2sea.com>
2025-01-04 18:55:23 +0100

    1st trial of R script: plotting and export the plot

---
+++ r_training/R/mins_playground.R
@@ -0,0 +1,40 @@
+# dependency declaration
+if (!require("ggplot2")) {
+  install.packages("ggplot2")
+}
+library(ggplot2)
+
+
+# PLAY WITH USEFUL package STATEMENTS
+# listing Installed Packages
+# installed.packages()
+
+# pkg <- installed.packages()
+# write.csv(pkg, "packages.csv")
+
+# PLAY WITH PLOTTING
+# below had issues for margin
+# par(mar = c(2, 2, 2, 2))  # Smaller margins
+# plot(1:10, main = "Simple Plot", xlab = "X-axis", ylab = "Y-axis")
+#
+# below had issues for margin
+# dev.new(width = 8, height = 6)  # Open a new plot window with specified size
+# plot(1:10, main = "Simple Plot", xlab = "X-axis", ylab = "Y-axis")
+
+# below works
+# explicitly open a larger plotting device, such as x11() (on Linux), windows() (on Windows), or quartz() (on macOS):
+# quartz(width = 8, height = 6)  # For macOS
+# plot(1:10, main = "Simple Plot", xlab = "X-axis", ylab = "Y-axis")
+
+# below works
+# Export to PDF
+pdf("plot_output.pdf", width = 8, height = 6)
+plot(1:10, main = "Simple Plot", xlab = "X-axis", ylab = "Y-axis")
+dev.off()
+# below works
+getwd()
+setwd("./r_training")
+# Export to PNG
+png("plot_output.png", width = 800, height = 600)
+plot(1:10, main = "Simple Plot", xlab = "X-axis", ylab = "Y-axis")
+dev.off()
