22c3bb183cb86244781bcd92ce3a4e2c493c8da0
min.shi <mish@source2sea.com>
2025-01-05 23:12:38 +0100

    wip: play with stats package

---
+++ r_training/R/mins_playground_statistics.R
@@ -0,0 +1,18 @@
+# Sample dataset
+data <- c(5, 8, 6, 9, 7, 10, 8, 6, 9, 10)
+
+# Mean, Median, Variance, and Standard Deviation
+mean(data)            # Mean
+median(data)          # Median
+var(data)             # Variance
+sd(data)              # Standard Deviation
+
+# Five-number summary
+summary(data)
+
+sort(data)
+
+# NOTE: in R console: help.start(), then search 'median' or nevigate to stats package; eg search for 'median'
+#     then get to a page similar to: http://127.0.0.1:15145/library/stats/html/median.html
+# OR, search median in https://www.rdocumentation.org/ and click the one from stats package to get here:
+#     https://www.rdocumentation.org/packages/stats/versions/3.6.2/topics/median
